e61df17c5d79eceff6621fae763e8d2270167ec2
Sebaastian <sfernandez1@uc.cl>
2018-06-16 13:35:44 -0400

    name course var

--- src/app/course-loader/course-loader.component.ts
+++ src/app/course-loader/course-loader.component.ts
@@ -8,6 +8,8 @@ import { Component, OnInit, ViewEncapsulation } from '@angular/core';
 })
 export class CourseLoaderComponent implements OnInit {
 
+  public nameCourse = '';
+
   constructor() { }
 
   ngOnInit() {
